b0ff13f3f4f19e43370e94fbd32a787f08bc0342
brnce <66827220+brnce@users.noreply.github.com>
2022-10-11 12:41:31 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bernice Esiemot");
+        System.out.println("53 Springroad");
+        System.out.println("07284829329");
+        System.out.println("Glasgow");
+
+    }
+}
